b152293e249cf5ca630efc0e1b19313201fc8a66
KAVINNESH A/L CHANDRA SHEKARAN <useb2241@gmail.com>
2026-04-21 01:25:03 +0800

    added a new webpage and features

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/Mainpage")
+def login():
+    return render_template("Mainpage.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
