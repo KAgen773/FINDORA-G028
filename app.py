24bebce5a59e174a44585a318d9a835c5698a9ca
KAVINNESH A/L CHANDRA SHEKARAN <useb2241@gmail.com>
2026-05-04 20:18:18 +0800

    Added database for login, register and item descriptions

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from flask import Flask, render_template, request, redirect, url_for
+import mysql.connector
+from flask import Flask, render_template, request, redirect, url_for, session
+
 
 app = Flask(__name__)
+app.secret_key = "findora_secret_key"  # Needed later for login sessions
 
-# Temporary database (in-memory)
-users = {}
+db = mysql.connector.connect(
+    host="sql12.freesqldatabase.com",
+    user="sql12825346",
+    password="Sa8Xc2HhZN",
+    database="sql12825346"
+)
+
+cursor = db.cursor(dictionary=True)
+
 
 # First page (login page)
 @app.route("/")
@@ -13,24 +23,31 @@ def interface():
 # Handle login
 @app.route('/login', methods=['POST'])
 def login():
-    username = request.form['username']
+    email = request.form['username']      #login field is email
     password = request.form['password']
 
+    sql = "SELECT * FROM users WHERE email = %s AND password = %s"
+    cursor.execute(sql, (email, password))
+    user = cursor.fetchone()
 
-    if username in users and users[username] == password:
+    if user:
+        session["user_id"] = user["id"]
         return redirect(url_for('mainpage'))
     else:
         return redirect(url_for('interface'))     #stay on login page
+       
     
 
 # Register page
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
-        username = request.form["username"]
+        email = request.form["email"]
         password = request.form["password"]
 
-        users[username] = password
+        sql = "INSERT INTO users (email,password) VALUES (%s, %s)"
+        cursor.execute(sql, (email, password))
+        db.commit()
 
         return redirect(url_for("interface"))    #back to login after register
 
@@ -43,3 +60,35 @@ def mainpage():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+@app.route("/report", methods=["POST"])
+def report():
+    if "user_id" not in session:
+        return redirect(url_for("interface"))
+    
+    item_name = request.form["item_name"]
+    location = request.form["location"]
+    description = request.form["description"]
+
+    sql = """
+    INSERT INTO items (user_id, type, name, description, location)
+    VALUES (%s, %s, %s, %s, %s)
+    """
+
+    #You need to know if its lost or found
+    #simplest fix: add hidden field in HTML (I'll show below)
+
+    item_type = request.form["type"]
+
+    cursor.execute(sql, (
+        session["user_id"],
+        item_type,
+        item_name,
+        description,
+        location,
+    ))
+
+    db.commit()
+
+    return redirect(url_for("mainpage"))
+
