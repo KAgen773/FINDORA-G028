1cd2d0f574e15a1bffcf40dc8590a22024ecd16a
SRI KAGENDRAN A/L CHANDRAN <srikagendran51@gmail.com>
2026-06-04 02:19:47 +0800

    perfect interface

--- app.py
+++ app.py
@@ -2,22 +2,46 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-# Fake login data (for now)
-USERNAME = "admin"
-PASSWORD = "12345"
+# Store users
+users = {
+    "admin": "12345"
+}
 
 # 🔹 First page (login page)
 @app.route('/')
 def login_page():
-    return render_template('index.html')
+    return render_template('login.html')
+
+# 🔹 Register page
+@app.route('/registerpage')
+def register_page():
+    return render_template('register.html')
+
+# 🔹 Handle register
+@app.route('/register', methods=['POST'])
+def register():
+
+    username = request.form['username']
+    password = request.form['password']
+
+    # Check if user exists
+    if username in users:
+        return "Username already exists!"
+
+    # Save new user
+    users[username] = password
+
+    return redirect(url_for('login_page'))
 
 # 🔹 Handle login
 @app.route('/login', methods=['POST'])
 def login():
+
     username = request.form['username']
     password = request.form['password']
 
-    if username == USERNAME and password == PASSWORD:
+    # Check registered users
+    if username in users and users[username] == password:
         return redirect(url_for('mainpage'))
     else:
         return "Invalid login. Try again."
@@ -29,3 +53,5 @@ def mainpage():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
